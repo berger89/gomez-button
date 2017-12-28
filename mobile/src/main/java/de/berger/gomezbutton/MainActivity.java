a59daf0c3c13dcac5eccfd4b44a7e309b8bfa09f
berger <berger.alexander89@gmail.com>
2017-12-28 20:51:55 +0100

    Updated mobile and wear layout

--- mobile/src/main/java/de/berger/gomezbutton/MainActivity.java
+++ mobile/src/main/java/de/berger/gomezbutton/MainActivity.java
@@ -145,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent();
                     intent.setAction(Intent.ACTION_VIEW);
                     intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    intent.setData(Uri.parse("https://shop.vfb.de/"));
+                    intent.setData(Uri.parse("https://shop.vfb.de/heimtrikot-17-18-2950-08.html"));
                     startActivity(intent);
                 } catch (ActivityNotFoundException e) {
                     Log.e(TAG, e.getMessage());
